1841c3c2054f697d94b57c39a2bcc0c6f8873198
Rayan Abou <ra_abo@live.concordia.ca>
2021-03-03 17:57:10 -0500

    Added endpoints for followers/following feature #19

--- Server/Controllers/FollowersController.cs
+++ Server/Controllers/FollowersController.cs
@@ -13,51 +13,184 @@ namespace Server.Controllers
     [ApiController]
     public class FollowersController : ControllerBase
     {
-        private readonly IFollowerServices _services = new FollowerServices();
+        private readonly DatabaseServices _databaseServices;
+
+        public FollowersController(IDatabaseSettings settings)
+        {
+            _databaseServices = new DatabaseServices(settings);
+        }
+
 
         [HttpGet]
-        [Route("GetFollowerItems")]
-        public ActionResult<Dictionary<long, MiniUser>> GetFollowerItems()
+        [Route("GetFollowers")]
+        public ActionResult<List<MiniUser>> GetFollowers(int id)
         {
-            var followerItems = _services.GetFollowerItems();
+            var user = _databaseServices.Get(id);
 
-            if (followerItems.Count == 0)
+            if (user == null)
             {
                 return NotFound();
             }
 
-            return followerItems;
+
+            return user.followers;
+        }
+
+        [HttpGet]
+        [Route("GetFollowings")]
+        public ActionResult<List<MiniUser>> GetFollowings(int id)
+        {
+            var user = _databaseServices.Get(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+
+            return user.following;
         }
 
         [HttpPost]
-        [Route("AddFollowerItems")]
-        public ActionResult<MiniUser> AddFollowerItems(MiniUser follower)
+        [Route("AddFollower")]
+        public ActionResult<User> AddFollower(int followerId, int userId)
         {
-            if (follower == null)
+            var user = _databaseServices.Get(userId);
+
+            if (user == null)
             {
                 return NotFound();
             }
-            var followerItems = _services.AddFollowerItems(follower);
 
-            return followerItems;
+            User followerUser = _databaseServices.Get(followerId);
+            
+            if (followerUser == null)
+            {
+                return BadRequest();
+            }
+
+            var follower = new MiniUser(followerUser.username, followerUser.userId, followerUser.profilePicture);
+            
+            if (user.followers.Contains(follower))
+            {
+                return Forbid();
+            }
+
+            user.followers.Add(follower);
+            
+            // Now Updating the follower's following list
+            var miniUser = new MiniUser(user.username, user.userId, user.profilePicture);
+
+            followerUser.following.Add(miniUser);
+
+            _databaseServices.Update(userId, user);
+            _databaseServices.Update(followerId, followerUser);
+
+            return user;
         }
 
         [HttpDelete]
-        [Route("DeleteFollowerItems")]
-        public ActionResult<Dictionary<long, Post>> DeleteFollowerItems(MiniUser follower)
+        [Route("RemoveFollower")]
+        public ActionResult<User> RemoveFollower(int followerId, int userId)
         {
+            var user = _databaseServices.Get(userId);
 
-            var followerItem = _services.GetFollowerItems();
-
-            if (followerItem.Count == 0 || follower == null || !followerItem.ContainsValue(follower))
+            if (user == null)
             {
-                return NoContent();
+                return NotFound();
             }
 
-            var followerItems = _services.RemoveFollowerItems(follower);
+            User followerUser = _databaseServices.Get(followerId);
+
+            if (followerUser == null)
+            {
+                return BadRequest();
+            }
+
+            if (user.followers.RemoveAll(follower => follower.userId == followerId) == 0)
+            {
+                return Forbid();
+            }
+
+            // Now Updating the follower's following list
+            followerUser.following.RemoveAll(user => user.userId == userId);
+
+            _databaseServices.Update(userId, user);
+            _databaseServices.Update(followerId, followerUser);
 
 
-            return Ok();
+            return user;
+        }
+
+        [HttpPost]
+        [Route("AddFollowing")]
+        public ActionResult<User> AddFollowing(int followingId, int userId)
+        {
+            var user = _databaseServices.Get(userId);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            User followingUser = _databaseServices.Get(followingId);
+
+            if (followingUser == null)
+            {
+                return BadRequest();
+            }
+
+            var following= new MiniUser(followingUser.username, followingUser.userId, followingUser.profilePicture);
+
+            if (user.following.Contains(following))
+            {
+                return Forbid();
+            }
+
+            user.following.Add(following);
+
+            // Now Updating the following's follower list
+            var miniUser = new MiniUser(user.username, user.userId, user.profilePicture);
+
+            followingUser.followers.Add(miniUser);
+
+            _databaseServices.Update(userId, user);
+            _databaseServices.Update(followingId, followingUser);
+
+
+            return user;
+        }
+
+        [HttpDelete]
+        [Route("RemoveFollowing")]
+        public ActionResult<User> RemoveFollowing(int followingId, int userId)
+        {
+            var user = _databaseServices.Get(userId);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            User followingUser = _databaseServices.Get(followingId);
+
+            if (followingUser == null)
+            {
+                return BadRequest();
+            }
+
+            if(user.following.RemoveAll(following => following.userId == followingId) == 0)
+            {
+                return Forbid();
+            }
+
+            // Now Updating the following's follower list
+            followingUser.followers.RemoveAll(user => user.userId == userId);
+
+            _databaseServices.Update(userId, user);
+            _databaseServices.Update(followingId, followingUser);
+
+            return user;
         }
     }
 }
