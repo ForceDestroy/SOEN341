ce713be7d0a68a55acd8a3e81a0f32b2e31d9e19
ForceDestroy <sangeethan.thaventhiran@gmail.com>
2021-03-28 17:33:25 -0400

    #48 Changed batch file

--- Server/StartServer.bat
+++ Server/StartServer.bat
@@ -3,4 +3,4 @@ SET LAUNCHER_PATH=bin\Debug\net5.0\Server.exe
 dir
 del "C:\Users\\My Documents\IISExpress\config"
 cd /d "C:\Program Files\IIS Express\"
-iisexpress.exe /site:"Server" /apppool:"Server AppPool" /trace:error
+iisexpress.exe /trace:error
