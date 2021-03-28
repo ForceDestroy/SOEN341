46a695741111c06527eb88a551037e10f9b3a43b
ForceDestroy <sangeethan.thaventhiran@gmail.com>
2021-03-28 17:08:21 -0400

    #48 Changed batch file and added another config file

--- Server/StartServer.bat
+++ Server/StartServer.bat
@@ -2,4 +2,4 @@ SET ASPNETCORE_ENVIRONMENT=Development
 SET LAUNCHER_PATH=bin\Debug\net5.0\Server.exe
 del "C:\Users\\My Documents\IISExpress\config"
 cd /d "C:\Program Files\IIS Express\"
-iisexpress.exe /config:"C:\Users\travis\build\ForceDestroy\SOEN341\Server\.vs\Server\config\applicationhost.config" /site:"Server" /apppool:"Server AppPool" /trace:error
+iisexpress.exe /config:"C:\Users\travis\build\ForceDestroy\SOEN341\Server\applicationhost.config" /site:"Server" /apppool:"Server AppPool" /trace:error
