1ab4105f7903a4182c47ec36f97369373fc3ceab
ForceDestroy <sangeethan.thaventhiran@gmail.com>
2021-03-28 17:29:26 -0400

    #48 Changed batch file

--- Server/StartServer.bat
+++ Server/StartServer.bat
@@ -3,4 +3,4 @@ SET LAUNCHER_PATH=bin\Debug\net5.0\Server.exe
 dir
 del "C:\Users\\My Documents\IISExpress\config"
 cd /d "C:\Program Files\IIS Express\"
-iisexpress.exe /config:"C:\Users\travis\build\ForceDestroy\SOEN341\Server\applicationhost.config" /site:"Server" /apppool:"Server AppPool" /trace:error
+iisexpress.exe /site:"Server" /apppool:"Server AppPool" /trace:error
