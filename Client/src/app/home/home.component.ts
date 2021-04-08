7b3cbca4a2a4ed45f5cb202db6a83fee280678b5
infernalturtle <jdiioia@outlook.com>
2021-04-08 17:51:25 -0400

    #14 homepage now shows the most recent posts of the people they are following

--- Client/src/app/home/home.component.ts
+++ Client/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { HomeService } from './home.service';
+import { ProfileService } from '../profile/profile.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  userName: string;
+  userId: string;
+  followPosts: any = [];
+  profileId: string;
+
+
+
+  constructor(private homeService: HomeService, private profileService: ProfileService, private route: ActivatedRoute) {
+    this.userId = localStorage.getItem('userId');
+    this.userName = localStorage.getItem('username');
   }
 
+  ngOnInit(): void {
+    this.route.url.subscribe(params => {
+      this.profileId = params[0].path;
+      this.loadPageContent();
+    })
+  }
+
+  loadPageContent() {
+    this.homeService.getFollowPosts(this.profileId).then((data) => {
+      console.log(this.profileId);
+      let follower = JSON.parse(data);
+      console.log(follower);
+      this.followPosts = follower.data;
+      console.log(this.followPosts);
+    })
+  }
 }
--- Client/src/app/home/home.module.ts
+++ Client/src/app/home/home.module.ts
@@ -1,19 +1,24 @@
 //Angular Modules
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 //Components
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
 
 //Material Imports
+import { MatDividerModule } from '@angular/material/divider';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/Icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
 
 
 const homeRoutes: Routes = [
-  {path: '', component: HomeComponent},
+  {path: ':id', component: HomeComponent},
 ];
 
 
@@ -22,8 +27,13 @@ const homeRoutes: Routes = [
   declarations: [HomeComponent],
   imports: [
     CommonModule,
+    HttpClientModule,
     SharedModule,
+    MatDividerModule,
     MatIconModule,
+    MatCardModule,
+    MatGridListModule,
+    MatButtonModule,
     MatToolbarModule,
     RouterModule.forChild(homeRoutes),
   ]
