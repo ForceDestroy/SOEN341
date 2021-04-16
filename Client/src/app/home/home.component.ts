8bc9121648048fee201f9bba7b1bf9708ccb721d
Simarjit Bilkhu <sim29j@gmail.com>
2021-04-15 21:47:06 -0400

    Added logout feature to home page

--- Client/src/app/home/home.component.ts
+++ Client/src/app/home/home.component.ts
@@ -22,7 +22,7 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    if (!this.verifyCanLogin()) {
+    if (this.verifyCanLogin()) {
       console.log("ERROR: Not logged in!");
       this.router.navigate(['../auth/login']);
     }
@@ -39,6 +39,19 @@ export class HomeComponent implements OnInit {
       })
 
     }
+  }
+
+  // Author: Simarjit bilkhu
+  // Logout
+  logout() : void{
+    //Remove localStorage details of current active session
+    const loginCredentials = localStorage.getItem('loginCredentials');
+    localStorage.removeItem(loginCredentials);
+
+    //Navigate to login page
+    this.router.navigate(['../auth/login']);
+
+    console.log("Goodbye!");
   }
 
   loadPageContent() {
